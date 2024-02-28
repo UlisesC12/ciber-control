fd884ec2eb159abd5147d0b7c77500a47eb4e7d0
UlisesC12 <ulisescandela99@gmail.com>
2024-02-27 21:27:45 -0600

    Create device grid component

---
+++ src/Components/DeviceGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DeviceGrid() {
+  return (
+    <div>
+      Estos son tus dispositivos:
+    </div>
+  );
+ }
+
+export {DeviceGrid}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Layout } from '../Layouts/Layout';
+import { DeviceGrid } from '../Components/DeviceGrid';
 
 function Home() {
   return (
    <Layout>
     <DeviceGrid/>
    </Layout>
-  );
- }
-
- function DeviceGrid() {
-  return (
-    <div>
-      Estos son tus dispositivos:
-    </div>
   );
  }
 
