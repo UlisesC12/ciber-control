a5ff2308d46cd7d7910477c59e8dbb8de6927e42
UlisesC12 <ulisescandela99@gmail.com>
2024-03-05 20:44:29 -0600

    add react context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { createContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
 
+const DeviceContext = createContext();
+
 function App() {
+  // const [devices, setDevices] = ()
  return (
-  <>
+  <DeviceContext.Provider value={[{
+    id: 1,
+    equipo: 'ASUS ROG',
+    estado: 0,
+    usuario: '',
+    tiempo: 0,
+  },{
+    id: 2,
+    equipo: 'ASUS ROG',
+    estado: 1,
+    usuario: 'Juan Garcia',
+    tiempo: 60,
+  }]}>
    <BrowserRouter>
     <Routes>
      <Route path="/" element={<Home />} />
     </Routes>
    </BrowserRouter>
    {/* <p className='font-bold text-3xl'>Hello!</p> */}
-  </>
+  </DeviceContext.Provider>
  );
 }
 
-export default App;
+export {App, DeviceContext};
--- src/Components/DeviceGrid.jsx
+++ src/Components/DeviceGrid.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DeviceContext } from '../App';
 
 function DeviceGrid() {
+  const devices = useContext(DeviceContext);
   return (
     <div>
       Estos son tus dispositivos:
+      {devices.id}
     </div>
   );
  }
